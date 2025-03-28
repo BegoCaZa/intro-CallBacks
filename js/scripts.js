73983c5fb5d3839184fd8f0b3adfe95ede1610c3
BegoCaZa <begoxcz@hotmail.com>
2025-03-28 11:52:04 +0100

    Update scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// Arrays métodos callback
+
+// 1️⃣ Camila está organizando datos en su sistema.
+// Crea una función que reciba un array de números y muestre por consola cada número multiplicado por su índice en el array.
+// Ejemplo: Si recibe [2, 5, 8], deberá mostrar:
+
+// 0 - 0  
+// 5 - 5  
+// 16 - 8
+
+//2️⃣ Bego quiere hacer cálculos avanzados.
+// Crea una función que reciba un array de números y devuelva un array con cada número dividido por su índice + 1 (el +1 es para evitar que se divida por 0).
+// Ejemplo: Si recibe [10, 20, 30], deberá imprimir [10, 10, 10].
